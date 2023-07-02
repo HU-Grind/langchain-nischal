fbb9f2d50c541f5a2f8d33571869ca9a0b7394db
Nischal-7 <nischalmadikiri10@gmail.com>
2023-07-02 21:11:36 +0530

    updated run.sh

--- quickStartGuide.py
+++ quickStartGuide.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
 #Load environment variables from .env file
 load_dotenv()
@@ -10,5 +11,21 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 llm = OpenAI(temperature=0.9) # the temperature ranges from 0 to 1 if 0, the model will be deterministic and if 1, the model will be more creative
 
-query = "What are 5 great vacation destinations in Karnataka during the month of August"
-print(llm(query))
+
+def simpleQuery():
+    query = "What are 5 great vacation destinations in Karnataka during the month of August"
+    print(llm(query))
+    
+def predict():
+    query = "What to name a company that makes colorful socks?"
+    print(llm.predict(query))
+    
+    
+def queryUsingPromptTemplate(favoriteFood):
+    prompt = PromptTemplate(
+        input_variables=["food"],
+        template="What are 5 vacations destinations in Karnataka for someone who likes to eat {food}",
+        )
+    print(llm(prompt.format(food=favoriteFood)))
+    
+queryUsingPromptTemplate("idly")
--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 python3 -m venv env
 source env/bin/activate
 pip install langchain openai python-dotenv
+python3 quickStartGuide.py
